115420a8edca6ff41a806365774961f089e902aa
Alaa Younsi <alaayounsi777@gmail.com>
2023-07-07 14:41:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $("#Main").slideDown(1000);
+
+});
+
+$(document).ready(function(){
+
+    $("#Pre").fadeIn(1000);
+
+});
+
+$(document).ready(function(){
+
+    $("#Pree").fadeIn(2000);
+
+});
